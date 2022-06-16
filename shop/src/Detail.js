e9de1c001c139ebd289abadd35a5549b519197d4
chajae <cjy4044@gmail.com>
2022-06-16 17:43:40 +0900

    Lifecycle Hook, useEffect

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,13 +1,12 @@
-import React from 'react';
 import {useNavigate, useParams} from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
+import React, { useEffect, useState } from 'react';
 
-let customDivComponents = styled.div`
+let CustomDivComponents = styled.div`
     padding : 20px;
 `;
-
-let customTitle = styled.h4`
+let CustomTitle = styled.h4`
     font-size : 25px;
     color : ${ props => props.color }
 `;
@@ -17,26 +16,42 @@ function Detail(props){
     let { id }= useParams();
     let navigate = useNavigate();
     let oSelectedItem = props.shoes.find(o => o.id == id);
+    let [alert,updateChanage] = useState(true);
+    let [sInput,updateInput] = useState("test");
+
+    useEffect(()=>{
+        let fTimer = setTimeout(()=>{
+            updateChanage(false);
+        },2000);
+
+        // Detail Component 가 Unmount 될때 실행됨
+        return ()=>{ clearTimeout(fTimer)}
+    },[]);
 
     return(
     <div className="container">
-            <customDivComponents>
-                <h4 className="red">Detail</h4>
-            </customDivComponents>
+        <CustomDivComponents>
+            <h4 className="red">Detail</h4>
+        </CustomDivComponents>
 
-            <div className="my-alert2">
+        {sInput} <br></br>
+        <input value={sInput} onChange={(e)=>{ updateInput(e.target.value) }}/>
+
+        {
+            alert &&  
+            <div className="my-alert">
                 <p>재고가 얼마 남지 않았습니다.</p>
             </div>
+        }
 
-            <div className="row">
-
-            <div className="col-md-6">
-                <img src={"https://codingapple1.github.io/shop/shoes"+(oSelectedItem.id+1)+".jpg"} with="100%"/>
+        <div className="row">
+            <div className="col-md-6 ">
+                <img src={"https://codingapple1.github.io/shop/shoes"+(oSelectedItem.id+1)+".jpg"} width="100%"/>
             </div>
+            
             <div className="col-md-6 mt-4">
                 <h4 className="pt-5">{oSelectedItem.title}</h4>
                 <p>{oSelectedItem.content}</p>
-
                 <p>{oSelectedItem.price}</p>
                 <button className="btn btn-danger">주문하기</button>
                 &nbsp;
